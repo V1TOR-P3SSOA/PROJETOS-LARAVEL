2a83389dc667f31054fcf7c7e98fd77b617438aa
V1TOR P <vitorpessoa36@gmail.com>
2025-09-14 10:17:37 -0300

    criação das routes diretas do CRUD

--- LabStock/routes/web.php
+++ LabStock/routes/web.php
@@ -3,8 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GlasswareController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [GlasswareController::class, 'index'])->name('index');
 
-Route::get('/glasswares', [GlasswareController::class, 'index']);
+Route::get('/glasswares', [GlasswareController::class, 'index'])->name('glasswares.index');
+
+Route::get('/glasswares/create', [GlasswareController::class, 'create'])->name('glasswares.create');
+
+Route::post('/glasswares', [GlasswareController::class, 'store'])->name('glasswares.store');
+
+Route::get('/glasswares/{glasswares}', [GlasswareController::class, 'show'])->name('glasswares.show');
+
+Route::get('/glasswares/{glasswares}/edit', [GlasswareController::class, 'edit'])->name('glasswares.edit');
+
+Route::get('/glasswares/{glasswares}', [GlasswareController::class, 'update'])->name('glasswares.update');
+
+Route::get('/glasswares/{glasswares}', [GlasswareController::class, 'destroy'])->name('glasswares.destroy');
