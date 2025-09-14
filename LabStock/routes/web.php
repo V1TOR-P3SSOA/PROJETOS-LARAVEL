01a29731600e560dca4524697e735c7e46b0aa17
V1TOR P <vitorpessoa36@gmail.com>
2025-09-14 11:30:51 -0300

    mudança de sintax das routes

--- LabStock/routes/web.php
+++ LabStock/routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GlasswareController;
 
-Route::get('/', [GlasswareController::class, 'index'])->name('index');
+Route::get('/', function () {
+    return view('index');
+})->name('index');
 
 Route::get('/glasswares', [GlasswareController::class, 'index'])->name('glasswares.index');
 
@@ -11,10 +13,10 @@ Route::get('/glasswares/create', [GlasswareController::class, 'create'])->name('
 
 Route::post('/glasswares', [GlasswareController::class, 'store'])->name('glasswares.store');
 
-Route::get('/glasswares/{glasswares}', [GlasswareController::class, 'show'])->name('glasswares.show');
+Route::get('/glasswares/{glassware}', [GlasswareController::class, 'show'])->name('glasswares.show');
 
-Route::get('/glasswares/{glasswares}/edit', [GlasswareController::class, 'edit'])->name('glasswares.edit');
+Route::get('/glasswares/{glassware}/edit', [GlasswareController::class, 'edit'])->name('glasswares.edit');
 
-Route::get('/glasswares/{glasswares}', [GlasswareController::class, 'update'])->name('glasswares.update');
+Route::get('/glasswares/{glassware}', [GlasswareController::class, 'update'])->name('glasswares.update');
 
-Route::get('/glasswares/{glasswares}', [GlasswareController::class, 'destroy'])->name('glasswares.destroy');
+Route::get('/glasswares/{glassware}', [GlasswareController::class, 'destroy'])->name('glasswares.destroy');
