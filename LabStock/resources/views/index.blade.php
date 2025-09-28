d7166039a65516e13d4de98bd14c782a6cd0410f
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-09-28 15:14:04 -0300

    criação da view index

---
+++ LabStock/resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LabStock</title>
+</head>
+<body>
+    <h1>Bem vindo(a) ao LabStock!</h1>
+    <p><i>"A organização é a base da ciência..."</i></p>
+
+    <h1>VIDRARIAS</h1>
+    <table>
+        <tr>
+            <td>NOME</td>
+            <td>LOTE</td>
+            <td>AÇÕES</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
