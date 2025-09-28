7e2d8ae2aa3c871912967c6158ac6b38feeed9dd
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-09-28 13:58:29 -0300

    criação das colunas da tabela glasswares

--- LabStock/database/migrations/2025_09_28_163639_create_glassware_table.php
+++ LabStock/database/migrations/2025_09_28_163639_create_glassware_table.php
@@ -6,22 +6,24 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('glassware', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("type");
+            $table->integer("capacity");
+            $table->integer("quantity");
+            $table->string("batch");
+            $table->text("notes");
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('glassware');
     }
+    
 };
