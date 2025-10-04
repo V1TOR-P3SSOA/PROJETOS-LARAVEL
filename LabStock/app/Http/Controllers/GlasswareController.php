03a3ae1be113b2d02908fb6524aa163f41e52d82
V1TOR-P3SSOA <vitor.freitas28@aluno.ce.gov.br>
2025-10-04 17:53:53 -0300

    remoção de comentários e criação do store

--- LabStock/app/Http/Controllers/GlasswareController.php
+++ LabStock/app/Http/Controllers/GlasswareController.php
@@ -13,50 +13,28 @@ class GlasswareController extends Controller
         $glasswares = Glassware::all();
         return view('glasswares.index', compact("glasswares"));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('glasswares.create');
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
-    }
+        Glassware::create($request->all());
 
-    /**
-     * Display the specified resource.
-     */
+        return redirect()->route('glasswares.index');
+    }
     public function show(string $id)
     {
         //
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
         //
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         //
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         //
